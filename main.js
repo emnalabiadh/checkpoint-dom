db9cf7d7fada9cd02f51776e49dc836bced334b1
emnalabiadh <emnalolita@gmail.com>
2021-01-12 09:44:24 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+/*let plus = document.querySelectorAll(".plus");
+console.log(plus);
+let qte = document.getElementsByClassName("qte");
+for (let i = 0; i < plus.length; i++) {
+  plus[i].addEventListener("click", function () {
+    qte[i].innerHTML++
+  });
+}
+
+let mince = document.querySelectorAll(".mince");
+let qte = document.getElementsByClassName("qte");
+for (let i = 0; i < mince.length; i++) {
+  mince[i].addEventListener("click", function () {
+    qte[i].innerHTML--
+  });*/
+}
+/*function myFunction() {
+  document.getElementById('demoInput').stepUp();
+}
+function myFunction() {
+  document.getElementById('demoInput').stepDown();
+}*/
+
+function myFunction() {
+  document.getElementById("heart").style.color = "red";
+}
+
+
+var card = document.getElementById('card');
+window.onclick = function(event) {
+  if (event.target == card) {
+    card.style.display = "none";
+  }
+}
+var card = document.getElementById('card');
+window.onclick = function(event) {
+  if (event.target == card) {
+    card.style.display = "none";
+  }
+}
+var card = document.getElementById('card');
+window.onclick = function(event) {
+  if (event.target == card) {
+    card.style.display = "none";
+  }
+}
+
+
+/*function myFunction() {
+  var x = document.getElementsByClassName("symbole");
+  x.style.color = "red";
+}*/
